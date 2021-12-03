cdb5702755e6c7c47ea9f97612f45b333e57d1a9
Baspar <me@bastien-laine.io>
2021-12-03 11:44:22 +0100

    Day3: Part1

--- go/day3/main.go
+++ go/day3/main.go
@@ -1,23 +1,63 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+
 	utils "github.com/baspar/adventofcode2021/internal"
 )
 
 type DayImpl struct {
-	input string
+	input [][]int
 }
 
 func (d *DayImpl) Init(input string) error {
-	d.input = input
+	lines := strings.Split(strings.Trim(input, "\n"), "\n")
+	for _, line := range lines {
+		var inputLine []int
+		for _, ch := range line {
+			if val, err := strconv.Atoi(string(ch)); err != nil {
+				return errors.New(fmt.Sprintf("Unknown value: %c", ch))
+			} else {
+				inputLine = append(inputLine, val)
+			}
+		}
+		d.input = append(d.input, inputLine)
+	}
+
+	if len(d.input) == 0 {
+		return errors.New("Didn't find any line in input")
+	}
+
 	return nil
 }
-func (d *DayImpl) Part1() (response string, err error) {
-	response = d.input
-	return
+func (d *DayImpl) Part1() (string, error) {
+	nbBits := len(d.input[0])
+
+	gammaRate := 0
+	epsilonRate := 0
+
+	for i := 0; i < nbBits; i++ {
+		hist := make(map[int]int)
+
+		for _, line := range d.input {
+			hist[line[i]] += 1
+		}
+
+		gammaRate *= 2
+		epsilonRate *= 2
+		if hist[1] > hist[0] {
+			gammaRate += 1
+		} else {
+			epsilonRate += 1
+		}
+	}
+
+	return fmt.Sprintf("%d", gammaRate*epsilonRate), nil
 }
 func (d *DayImpl) Part2() (response string, err error) {
-	response = d.input
 	return
 }
 
--- go/day3/main_test.go
+++ go/day3/main_test.go
@@ -6,27 +6,50 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-const input = ``
+const input = `00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010`
 
 func TestPart1(t *testing.T) {
+	var (
+		err error
+		res string
+	)
 	assert := assert.New(t)
 
 	d := &DayImpl{}
-	d.Init(input)
+	
+	err = d.Init(input)
+	assert.Nil(err)
 
-	res, err := d.Part1()
+	res, err = d.Part1()
 
-	assert.Equal("", res)
+	assert.Equal("198", res)
 	assert.Nil(err)
 }
 
 func TestPart2(t *testing.T) {
+	var (
+		err error
+		res string
+	)
 	assert := assert.New(t)
 
 	d := &DayImpl{}
-	d.Init(input)
+	
+	err = d.Init(input)
+	assert.Nil(err)
 
-	res, err := d.Part2()
+	res, err = d.Part2()
 
 	assert.Equal("", res)
 	assert.Nil(err)
