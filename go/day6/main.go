26c29c8555e4ed963dad5d008000fb62e6570a1d
Baspar <me@bastien-laine.io>
2021-12-06 11:19:53 +0100

    Day6: Part 1&2

--- go/day6/main.go
+++ go/day6/main.go
@@ -1,20 +1,67 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+
 	utils "github.com/baspar/adventofcode2021/internal"
 )
 
 type DayImpl struct {
+	fishes [9]int64
 }
 
 func (d *DayImpl) Init(lines []string) error {
+	var (
+		t   int
+		err error
+	)
+
+	for i := range d.fishes {
+		d.fishes[i] = 0
+	}
+
+	for _, timer := range strings.Split(lines[0], ",") {
+		t, err = strconv.Atoi(timer)
+		if err != nil {
+			return fmt.Errorf("Cannot parse '%s' to int", timer)
+		}
+		if t < 0 || t > 8 {
+			return fmt.Errorf("'%d' is out of the limit", t)
+		}
+
+		d.fishes[t] += 1
+	}
 	return nil
 }
+func (d *DayImpl) runOneDay() {
+	newFishes := d.fishes[0]
+	for i := 1; i<len(d.fishes); i++ {
+		d.fishes[i-1] = d.fishes[i]
+	}
+	d.fishes[6] += newFishes
+	d.fishes[8] = newFishes
+}
 func (d *DayImpl) Part1() (string, error) {
-	return "", nil
+	for i := 0; i < 80; i++ {
+		d.runOneDay()
+	}
+	var total int64 = 0
+	for _, nb := range d.fishes {
+		total += nb
+	}
+	return fmt.Sprint(total), nil
 }
 func (d *DayImpl) Part2() (string, error) {
-	return "", nil
+	for i := 0; i < 256; i++ {
+		d.runOneDay()
+	}
+	var total int64 = 0
+	for _, nb := range d.fishes {
+		total += nb
+	}
+	return fmt.Sprint(total), nil
 }
 
 func main() {
--- go/day6/main_test.go
+++ go/day6/main_test.go
@@ -7,28 +7,28 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var input = utils.SanitizeInput(``)
+var input = utils.SanitizeInput(`3,4,3,1,2`)
+
+var d = &DayImpl{}
 
 func TestPart1(t *testing.T) {
 	assert := assert.New(t)
 
-	d := &DayImpl{}
 	d.Init(input)
 
 	res, err := d.Part1()
 
-	assert.Equal("", res)
+	assert.Equal("5934", res)
 	assert.Nil(err)
 }
 
 func TestPart2(t *testing.T) {
 	assert := assert.New(t)
 
-	d := &DayImpl{}
 	d.Init(input)
 
 	res, err := d.Part2()
 
-	assert.Equal("", res)
+	assert.Equal("26984457539", res)
 	assert.Nil(err)
 }
