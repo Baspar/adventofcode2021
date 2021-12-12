f6e15898cbeb78baa7aa71af11672ce0f2debf4b
Baspar <me@bastien-laine.io>
2021-12-12 14:13:40 +0100

    Day12: Part1

--- go/day12/main.go
+++ go/day12/main.go
@@ -1,17 +1,79 @@
 package main
 
 import (
+	"container/list"
+	"fmt"
+	"strings"
+
 	utils "github.com/baspar/adventofcode2021/internal"
 )
 
+func isSmall(cave string) bool {
+	return cave[0] >= 'a' && cave[0] <= 'z'
+}
+
 type DayImpl struct {
+	caves map[string][]string
+}
+
+type ExplNode struct {
+	currentCave string
+	visited     map[string]bool
+}
+
+func NewExplNode(cave string) ExplNode {
+	visited := make(map[string]bool)
+	if isSmall(cave) {
+		visited[cave] = true
+	}
+
+	return ExplNode{cave, visited}
+}
+
+func (e *ExplNode) MoveTo(nextCave string) ExplNode {
+	explNode := NewExplNode(nextCave)
+
+	for cave := range e.visited {
+		explNode.visited[cave] = true
+	}
+
+	return explNode
 }
 
 func (d *DayImpl) Init(lines []string) error {
+	d.caves = make(map[string][]string)
+	for _, line := range lines {
+		caves := strings.Split(line, "-")
+		if len(caves) < 2 {
+			return fmt.Errorf("Cannot parse line %s", line)
+		}
+
+		d.caves[caves[0]] = append(d.caves[caves[0]], caves[1])
+		d.caves[caves[1]] = append(d.caves[caves[1]], caves[0])
+	}
 	return nil
 }
 func (d *DayImpl) Part1() (string, error) {
-	return "", nil
+	var q list.List
+	q.PushBack(NewExplNode("start"))
+
+	numberOfPath := 0
+	for q.Len() > 0 {
+		node := q.Remove(q.Front()).(ExplNode)
+
+		if node.currentCave == "end" {
+			numberOfPath++
+		}
+
+		for _, neighbourCave := range d.caves[node.currentCave] {
+			_, visited := node.visited[neighbourCave]
+			if !visited {
+				q.PushBack(node.MoveTo(neighbourCave))
+			}
+		}
+	}
+
+	return fmt.Sprintf("%d", numberOfPath), nil
 }
 func (d *DayImpl) Part2() (string, error) {
 	return "", nil
--- go/day12/main_test.go
+++ go/day12/main_test.go
@@ -7,28 +7,75 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var input = utils.SanitizeInput(``)
+var input1 = utils.SanitizeInput(`start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end`)
+var input2 = utils.SanitizeInput(`dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc`)
+var input3 = utils.SanitizeInput(`fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW`)
+
+type TestSet struct {
+	input []string
+	expected string
+}
 
 var d = &DayImpl{}
 
 func TestPart1(t *testing.T) {
+	inputs := []TestSet{
+		{input1, "10"},
+		{input2, "19"},
+		{input3, "226"},
+	}
+
 	assert := assert.New(t)
 
-	d.Init(input)
+	for _, testSet := range inputs {
+		d.Init(testSet.input)
 
-	res, err := d.Part1()
+		res, err := d.Part1()
 
-	assert.Equal("", res)
-	assert.Nil(err)
+		assert.Equal(testSet.expected, res)
+		assert.Nil(err)
+	}
 }
 
-func TestPart2(t *testing.T) {
-	assert := assert.New(t)
-
-	d.Init(input)
-
-	res, err := d.Part2()
-
-	assert.Equal("", res)
-	assert.Nil(err)
-}
+// func TestPart2(t *testing.T) {
+// 	assert := assert.New(t)
+//
+// 	d.Init(input)
+//
+// 	res, err := d.Part2()
+//
+// 	assert.Equal("", res)
+// 	assert.Nil(err)
+// }
