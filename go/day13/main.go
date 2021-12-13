a8c49cdd4315b9d9cf19c36002d74ceb361bf1c1
Baspar <me@bastien-laine.io>
2021-12-13 14:43:14 +0100

    Day13: Part1

--- go/day13/main.go
+++ go/day13/main.go
@@ -1,17 +1,90 @@
 package main
 
 import (
+	"fmt"
+	"strings"
+
 	utils "github.com/baspar/adventofcode2021/internal"
 )
 
-type DayImpl struct {
+type Grid map[int]map[int]bool
+
+func (g *Grid) add(x, y int) {
+	if g == nil {
+		*g = make(Grid)
+	}
+	if _, exists := (*g)[x]; !exists {
+		(*g)[x] = make(map[int]bool)
+	}
+	(*g)[x][y] = true
 }
 
+type Fold struct {
+	isVertical bool
+	coord      int
+}
+type DayImpl struct {
+	g     Grid
+	folds []Fold
+}
+
+func (d *DayImpl) fold(i int) {
+	newG := make(Grid)
+	fold := d.folds[i]
+
+	for x, line := range d.g {
+		for y := range line {
+			if fold.isVertical && x > fold.coord {
+				x = 2*fold.coord - x
+			}
+
+			if !fold.isVertical && y > fold.coord {
+				y = 2*fold.coord - y
+			}
+
+			newG.add(x, y)
+		}
+	}
+	d.g = newG
+}
 func (d *DayImpl) Init(lines []string) error {
+	readingFold := false
+	d.g = make(Grid)
+	for _, line := range lines {
+		if line == "" {
+			readingFold = true
+			continue
+		}
+
+		if readingFold {
+			tokens := strings.Split(line, " ")
+			var (
+				dir   rune
+				coord int
+			)
+			token := tokens[len(tokens)-1]
+			if _, err := fmt.Sscanf(token, "%c=%d", &dir, &coord); err != nil {
+				return fmt.Errorf("Cannot read fold for '%s': %w", token, err)
+			}
+			d.folds = append(d.folds, Fold{isVertical: dir == 'x', coord: coord})
+		} else {
+			var x, y int
+			if _, err := fmt.Sscanf(line, "%d,%d", &x, &y); err != nil {
+				return fmt.Errorf("Cannot read Cell for '%s': %w", line, err)
+			}
+			d.g.add(x, y)
+		}
+	}
+
 	return nil
 }
 func (d *DayImpl) Part1() (string, error) {
-	return "", nil
+	d.fold(0)
+	tot := 0
+	for _, line := range d.g {
+		tot += len(line)
+	}
+	return fmt.Sprintf("%d", tot), nil
 }
 func (d *DayImpl) Part2() (string, error) {
 	return "", nil
--- go/day13/main_test.go
+++ go/day13/main_test.go
@@ -7,7 +7,27 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var input = utils.SanitizeInput(``)
+var input = utils.SanitizeInput(`6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5`)
 
 var d = &DayImpl{}
 
@@ -18,7 +38,7 @@ func TestPart1(t *testing.T) {
 
 	res, err := d.Part1()
 
-	assert.Equal("", res)
+	assert.Equal("17", res)
 	assert.Nil(err)
 }
 
