b37548f831d3fd0ef7118f2e200fc872168ac05f
Baspar <me@bastien-laine.io>
2021-12-16 23:26:30 +0100

    Day15: Fix part2

--- go/day15/main.go
+++ go/day15/main.go
@@ -14,9 +14,8 @@ type Position struct {
 	y int
 }
 type Expl struct {
-	position     Position
-	prevPosition Position
-	score        int
+	position Position
+	score    int
 }
 
 type Heap []Expl
@@ -34,10 +33,10 @@ func (h *Heap) Pop() interface{} {
 
 type Grid [][]int
 func (g Grid) getNeighbourOf(pos Position, scale int) (out []Position) {
-	if pos.x-1 > 0 {
+	if pos.x-1 >= 0 {
 		out = append(out, Position{pos.x - 1, pos.y})
 	}
-	if pos.y-1 > 0 {
+	if pos.y-1 >= 0 {
 		out = append(out, Position{pos.x, pos.y - 1})
 	}
 	if pos.x+1 < scale*len(g) {
@@ -56,66 +55,35 @@ func (g Grid) getScoreOf(pos Position, scale int) int {
 	return val
 }
 func (g Grid) dijkstra(initialPosition Position, scale int) (string, error) {
-	previousPosition := make(map[Position]Position)
+	seen := make(map[Position]bool)
 
 	q := &Heap{}
-	heap.Init(q)
-	heap.Push(q, Expl{initialPosition, initialPosition, 0})
+	heap.Push(q, Expl{initialPosition, 0})
 
-	endingX, endingY := scale*len(g)-1, scale*len(g[0])-1
+	target := Position{scale*len(g) - 1, scale*len(g[0]) - 1}
 
 	for q.Len() > 0 {
 		expl := heap.Pop(q).(Expl)
 
-		if _, exists := previousPosition[expl.position]; exists {
+		if seen[expl.position] {
 			continue
 		}
 
-		previousPosition[expl.position] = expl.prevPosition
-
-		if expl.position.x == endingX && expl.position.y == endingY {
+		if expl.position == target {
 			return fmt.Sprintf("%d", expl.score), nil
 		}
 
+		seen[expl.position] = true
 		for _, pos := range g.getNeighbourOf(expl.position, scale) {
-			heap.Push(q, Expl{pos, expl.position, expl.score + g.getScoreOf(pos, scale)})
+			heap.Push(q, Expl{pos, expl.score + g.getScoreOf(pos, scale)})
 		}
 	}
 	return "", errors.New("Cannot find path to last cell")
-}
-func (g Grid) str(prevPosition map[Position]Position, scale int) string {
-	isInPath := make(map[Position]bool)
-	pos := Position{scale*len(g)-1, scale*len(g[0])-1}
-	for {
-		isInPath[pos] = true
-		if prevPosition[pos] == pos {
-			break
-		}
-		pos=prevPosition[pos]
-	}
-
-	s := ""
-	width, height := len(g)*scale, len(g[0])*scale
-	for x := 0; x < width; x++ {
-		for y := 0; y < height; y++ {
-			c := fmt.Sprint(g.getScoreOf(Position{x, y}, scale))
-			if isInPath[Position{x, y}] {
-				c = fmt.Sprintf("\033[1;32m%s\033[0m", c)
-			} else {
-				c = fmt.Sprintf("\033[1;33m%s\033[0m", c)
-			}
-			s += c
-		}
-		s += "\n"
-	}
-
-	return s
 }
 
 type DayImpl struct {
 	grid Grid
 }
-
 func (d *DayImpl) Init(lines []string) error {
 	d.grid = make(Grid, len(lines))
 	for x, line := range lines {
