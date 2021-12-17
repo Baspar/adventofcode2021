d84f4612a5a8104a050f213fd92daf2f55c1cbdc
Baspar <me@bastien-laine.io>
2021-12-17 18:37:43 +0100

    Day17: Part 1&2

--- go/day17/main.go
+++ go/day17/main.go
@@ -1,20 +1,97 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+
 	utils "github.com/baspar/adventofcode2021/internal"
+	"github.com/baspar/adventofcode2021/internal/math"
 )
 
+type Position struct {
+	x  int
+	y  int
+	dx int
+	dy int
+}
+
+func (p *Position) step() {
+	p.x += p.dx
+	p.y += p.dy
+	p.dy--
+	if p.dx > 0 {
+		p.dx--
+	} else if p.dx < 0 {
+		p.dx++
+	}
+}
+func (p *Position) run(t Target) (maxHeight int, err error) {
+	maxHeight = p.y
+	for {
+		maxHeight = math.Max(maxHeight, p.y)
+		if t.contains(*p) {
+			return maxHeight, nil
+		} else if t.overshot(*p) {
+			return 0, errors.New("Overshot")
+		} else if t.undershot(*p) {
+			return 0, errors.New("Undershot")
+		}
+
+		p.step()
+	}
+}
+
+type Target struct {
+	x1 int
+	x2 int
+	y1 int
+	y2 int
+}
+
+func (t Target) contains(pos Position) bool {
+	return pos.x >= t.x1 &&
+		pos.y >= t.y1 &&
+		pos.x <= t.x2 &&
+		pos.y <= t.y2
+}
+func (t Target) overshot(pos Position) bool {
+	return pos.x > t.x2
+}
+func (t Target) undershot(pos Position) bool {
+	return pos.y < t.y1
+}
+
 type DayImpl struct {
+	target Target
 }
 
 func (d *DayImpl) Init(lines []string) error {
-	return nil
+	_, err := fmt.Sscanf(lines[0], "target area: x=%d..%d, y=%d..%d", &d.target.x1, &d.target.x2, &d.target.y1, &d.target.y2)
+	return err
 }
 func (d *DayImpl) Part1() (string, error) {
-	return "", nil
+	maxHeight := 0
+	for dx := 0; dx < 1000; dx++ {
+		for dy := 0; dy < 1000; dy++ {
+			p := Position{0, 0, dx, dy}
+			if max, err := p.run(d.target); err == nil {
+				maxHeight = math.Max(maxHeight, max)
+			}
+		}
+	}
+	return fmt.Sprint(maxHeight), nil
 }
 func (d *DayImpl) Part2() (string, error) {
-	return "", nil
+	tot := 0
+	for dx := -1000; dx < 1000; dx++ {
+		for dy := -1000; dy < 1000; dy++ {
+			p := Position{0, 0, dx, dy}
+			if _, err := p.run(d.target); err == nil {
+				tot++
+			}
+		}
+	}
+	return fmt.Sprint(tot), nil
 }
 
 func main() {
--- go/day17/main_test.go
+++ go/day17/main_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var input = utils.SanitizeInput(``)
+var input = utils.SanitizeInput(`target area: x=20..30, y=-10..-5`)
 
 var d = &DayImpl{}
 
@@ -18,7 +18,7 @@ func TestPart1(t *testing.T) {
 
 	res, err := d.Part1()
 
-	assert.Equal("", res)
+	assert.Equal("45", res)
 	assert.Nil(err)
 }
 
@@ -29,6 +29,6 @@ func TestPart2(t *testing.T) {
 
 	res, err := d.Part2()
 
-	assert.Equal("", res)
+	assert.Equal("112", res)
 	assert.Nil(err)
 }
